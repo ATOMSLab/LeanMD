e98c27aa2e9014ebfecf7a8b5b3c0a8ff1ee5e08
Oscar M <143578027+Oscario0@users.noreply.github.com>
2025-04-09 16:41:48 -0400

    update ljc

--- lakefile.lean
+++ lakefile.lean
@@ -6,11 +6,13 @@ package "LeanMD" where
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
   ]
-  -- add any additional package configuration options here
+  
+    -- add any additional package configuration options here{
 
+  -- add any additional package configuration options here
 require "leanprover-community" / "mathlib"
+
 
 @[default_target]
 lean_lib «LeanMD» where
   -- add any library configuration options here
-bbbb
