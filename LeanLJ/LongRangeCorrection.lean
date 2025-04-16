aa664bc315ea26bd3de5c343b74944175ead9451
Ejike89 <ugwuanyiejike89@gmail.com>
2025-04-16 12:21:23 -0400

    Update LongRangeCorrection.lean

--- LeanLJ/LongRangeCorrection.lean
+++ LeanLJ/LongRangeCorrection.lean
@@ -2,10 +2,11 @@ import Mathlib
 
 open Real MeasureTheory
 
-def U_LRC (ρ ε σ rc τ : α) [Field α] : α :=
+
+def U_LRC {α : Type} [Field α] (ρ ε σ rc τ : α) : α :=
   (8 * τ * ρ * ε) * ((1/9) * (σ ^ 12 / rc ^ 9) - (1/3) * (σ ^ 6 / rc ^ 3))
 
-theorem long_range_correction_equality (hr : 0 < rc) (ρ ε σ : ℝ) :
+theorem long_range_correction_equality  (rc ρ ε σ : ℝ) (hr : 0 < rc) :
     (2 * π * ρ) * ∫ (r : ℝ) in Set.Ioi rc, 4 * ε * (r ^ 2 * (((σ / r) ^ 12) -
     ((σ / r) ^ 6))) = (8 * π * ρ * ε) * ((1/9) * (σ ^ 12 / rc ^ 9) -
     (1/3) * (σ ^ 6 / rc ^ 3)) := by
