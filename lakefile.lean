750cc343b6e24c22bc52a6af98a8e3bb0c9d07ae
ATOMSLab <82960081+ATOMSLab@users.noreply.github.com>
2025-04-19 19:32:40 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-package "LeanMD" where
+package "LeanLJ" where
   -- Settings applied to both builds and interactive editing
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
@@ -14,5 +14,5 @@ require "leanprover-community" / "mathlib"
 
 
 @[default_target]
-lean_lib «LeanMD» where
+lean_lib «LeanLJ» where
   -- add any library configuration options here
