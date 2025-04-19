09e5d8603b33ee03d00093f504807abb4d8b030c
ATOMSLab <82960081+ATOMSLab@users.noreply.github.com>
2025-04-19 18:41:18 -0400

    Update LeanLJ.lean

--- LeanLJ.lean
+++ LeanLJ.lean
@@ -1,3 +1,3 @@
--- This module serves as the root of the `LeanMD` library.
+-- This module serves as the root of the `LeanLJ` library.
 -- Import modules here that should be built as part of the library.
-import LeanMD.Basic   
+import LeanLJ.Basic
