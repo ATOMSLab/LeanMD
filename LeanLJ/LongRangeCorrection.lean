0ed1370013f2e9c96a3b34df252e266aee2d652f
Ejike89 <ugwuanyiejike89@gmail.com>
2025-04-25 00:56:29 -0400

    Update LongRangeCorrection.lean

--- LeanLJ/LongRangeCorrection.lean
+++ LeanLJ/LongRangeCorrection.lean
@@ -95,10 +95,7 @@ theorem long_range_correction_equality_polymorphic
     (2 * π * ρ) * ∫ (r : ℝ) in Set.Ioi rc,
       4 * ε * (r ^ 2 * (((σ / r) ^ 12) - ((σ / r) ^ 6))) =
     U_LRC ρ π ε σ rc := by
-  have : U_LRC ρ π ε σ rc = U_LRC_real ρ ε σ rc := by
-    unfold U_LRC U_LRC_real
-    ring
-  rw [this]
+  have : U_LRC ρ π ε σ rc = U_LRC_real ρ ε σ rc :=  by apply U_LRC_eq_U_LRC_real 
   exact long_range_correction_equality' rc ρ ε σ hr
 
   
