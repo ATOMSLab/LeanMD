cd8959a1147912c5f699e24debf45c0ec82ef06f
Ejike89 <ugwuanyiejike89@gmail.com>
2025-05-02 12:44:18 -0400

    Update Definitions.lean

--- LeanLJ/Definitions.lean
+++ LeanLJ/Definitions.lean
@@ -51,20 +51,20 @@ def lj_p {α : Type} [RealLike α] (r r_c ε σ : α) : α :=
     0
 
 -- Polymorphic definition of Periodic boundary condition
-def pbc (position boxLength : α) [RealLike α] : α :=
-  position - boxLength * (HasRound.pround (position / boxLength) : α)
+def pbc (position box_length : α) [RealLike α] : α :=
+  position - box_length * (HasRound.pround (position / box_length) : α)
 
 -- Float type definition of periodic boundary conditions
-def pbc_float (position boxLength : Float) : Float :=
-  position - boxLength * Float.round (position / boxLength)
+def pbc_Float (position box_length : Float) : Float :=
+  position - box_length * Float.round (position / box_length)
 
 -- Real type definition of periodic boundary conditions
-noncomputable def pbc_Real (position boxLength : ℝ) : ℝ :=
-  position - boxLength * round (position / boxLength)
+noncomputable def pbc_Real (position box_length : ℝ) : ℝ :=
+  position - box_length * round (position / box_length)
 
 
 -- Float type definition of Lennard-Jones potential
-def lj_float (r r_c ε σ : Float) : Float :=
+def lj_Float (r r_c ε σ : Float) : Float :=
     if r ≤ r_c then
       let r3 := (σ / r) ^ (3 : Nat)
       let r6 := r3 * r3
@@ -84,30 +84,30 @@ noncomputable def lj_Real  (r r_c ε σ  : ℝ) : ℝ :=
     0
 
 -- Polymorphic definition of Minimum image distance in 3 dimensions, simplified
-def MinImageDistance {α : Type } [RealLike α] (boxLength posA posB : Fin 3 → α)  : α :=
-  let dist := fun i => pbc (posB i - posA i) (boxLength i)
+def MinImageDistance {α : Type } [RealLike α] (box_length posA posB : Fin 3 → α)  : α :=
+  let dist := fun i => pbc (posB i - posA i) (box_length i)
   HasSqrt.sqrt (Finset.univ.sum (fun i => (dist i) ^ (2 : Nat)))
 
 -- Polymorphic definition of Minimum image distance in 3 dimensions
 def minImageDistance {α : Type } [RealLike α]
-    (posA posB : Fin 3 → α) (boxLength : Fin 3 → α) : α :=
-  let dx := pbc (posB (0:Fin 3) - posA (0:Fin 3)) (boxLength (0:Fin 3))
-  let dy := pbc (posB (1:Fin 3) - posA (1:Fin 3)) (boxLength (1:Fin 3))
-  let dz := pbc (posB (2:Fin 3) - posA (2:Fin 3)) (boxLength (2:Fin 3))
+    (posA posB : Fin 3 → α) (box_length : Fin 3 → α) : α :=
+  let dx := pbc (posB (0:Fin 3) - posA (0:Fin 3)) (box_length (0:Fin 3))
+  let dy := pbc (posB (1:Fin 3) - posA (1:Fin 3)) (box_length (1:Fin 3))
+  let dz := pbc (posB (2:Fin 3) - posA (2:Fin 3)) (box_length (2:Fin 3))
   HasSqrt.sqrt (dx ^ (2 : Nat) + dy ^ (2 : Nat) + dz ^ (2 : Nat))
 
 -- Real type definition of Minimum image distance
-noncomputable def minImageDistance_Real (posA posB : Fin 3 → ℝ) (boxLength : Fin 3 → ℝ) : ℝ :=
-  let dx := pbc_Real (posB (0:Fin 3) - posA (0:Fin 3)) (boxLength (0:Fin 3))
-  let dy := pbc_Real (posB (1:Fin 3) - posA (1:Fin 3)) (boxLength (1:Fin 3))
-  let dz := pbc_Real (posB (2:Fin 3) - posA (2:Fin 3)) (boxLength (2:Fin 3))
+noncomputable def minImageDistance_Real (posA posB : Fin 3 → ℝ) (box_length : Fin 3 → ℝ) : ℝ :=
+  let dx := pbc_Real (posB (0:Fin 3) - posA (0:Fin 3)) (box_length (0:Fin 3))
+  let dy := pbc_Real (posB (1:Fin 3) - posA (1:Fin 3)) (box_length (1:Fin 3))
+  let dz := pbc_Real (posB (2:Fin 3) - posA (2:Fin 3)) (box_length (2:Fin 3))
   HasSqrt.sqrt (dx ^ (2 : Nat) + dy ^ (2 : Nat) + dz ^ (2 : Nat))
 
 -- Float type definition of Minimum image distance
-def minImageDistance_float (posA posB : Fin 3 → Float) (boxLength : Fin 3 → Float) : Float :=
-  let dx := pbc_float (posB (0:Fin 3) - posA (0:Fin 3)) (boxLength (0:Fin 3))
-  let dy := pbc_float (posB (1:Fin 3) - posA (1:Fin 3)) (boxLength (1:Fin 3))
-  let dz := pbc_float (posB (2:Fin 3) - posA (2:Fin 3)) (boxLength (2:Fin 3))
+def minImageDistance_float (posA posB : Fin 3 → Float) (box_length : Fin 3 → Float) : Float :=
+  let dx := pbc_Float (posB (0:Fin 3) - posA (0:Fin 3)) (box_length (0:Fin 3))
+  let dy := pbc_Float (posB (1:Fin 3) - posA (1:Fin 3)) (box_length (1:Fin 3))
+  let dz := pbc_Float (posB (2:Fin 3) - posA (2:Fin 3)) (box_length (2:Fin 3))
   Float.sqrt (dx ^ (2 : Nat) + dy ^ (2 : Nat) + dz ^ (2 : Nat))
 
 #eval HasSqrt.sqrt (5:Float)
